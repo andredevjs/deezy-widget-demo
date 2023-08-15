eeb49d33e7de71e7229adb8928d1ad5fed9e66c0
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-08-14 21:02:04 -0600

    fix: deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import { DeezyWidget } from "deezy-widget";
 import "./App.css";
 console.log(DeezyWidget);
 
 function App() {
+  useEffect(() => {
+    console.log("loaded");
+  }, [DeezyWidget]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
+        {DeezyWidget && (
+          <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
+        )}
       </header>
     </div>
   );
