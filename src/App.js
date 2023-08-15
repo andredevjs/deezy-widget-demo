ea8fb304c983026a9f44bffe41f6e5cbb0681443
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-08-14 20:10:42 -0600

    fix: deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import * as lib from "deezy-widget";
+const { DeezyWidget } = lib;
+console.log(lib);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
