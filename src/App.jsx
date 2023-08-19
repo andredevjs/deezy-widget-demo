0a0e2db1857ae07436b55d1bebaded4f78290e07
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-08-18 18:18:29 -0600

    feat: update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { DeezyWidget } from "deezy-widget";
 // TODO: FIX
 
-// import "deezy-widget/dist/style.css";
+import "deezy-widget/dist/style.css";
 import "./App.css";
 console.log("Loading widget", DeezyWidget);
 console.log(DeezyWidget);
@@ -10,9 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {DeezyWidget && (
-          <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
-        )}
+        <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
       </header>
     </div>
   );
