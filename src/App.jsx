643530b29e1952563afdfc5d444216bfa338c41b
andredevjs <97211514+andredevjs@users.noreply.github.com>
2023-08-22 14:40:27 -0600

    feat: library

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
-import { DeezyWidget } from "deezy-widget";
+import {
+  DeezyWidget,
+  InscriptionPreview,
+  BidWidget,
+  ListBids,
+} from "deezy-widget";
 // TODO: FIX
 
 import "deezy-widget/dist/style.css";
 import "./App.css";
-console.log("Loading widget", DeezyWidget);
-console.log(DeezyWidget);
 
 function App() {
+  const inscriptionId =
+    "97b58013212c065c78e66e3c69ea0750396a6c7e84578da03c06362f8f526501i0";
   return (
     <div className="App">
-      <header className="App-header">
-        <DeezyWidget inscriptionId="b312d2ac6ed1fee29acb6be9d1cf07e001595a6e9a1a26fef324552da290e343i0" />
-      </header>
+      <section className="section-auction">
+        <div className="w-container">
+          <div className="auction-container">
+            <div className="column preview">
+              <div className="img-container">
+                <div className="img-preview">
+                  <InscriptionPreview
+                    utxo={{
+                      inscriptionId,
+                      contentType: "image/webp",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="column info">
+              <div className="inscription-info-container">
+                <div className="inscription-info">
+                  <p className="inscription-title">Babe 404</p>
+                </div>
+                <div>
+                  <DeezyWidget useBidding inscriptionId={inscriptionId} />
+                </div>
+                <div className="bid-form">
+                  <BidWidget
+                    inscriptionId={inscriptionId}
+                    suggestedPrice={1000}
+                  />
+                </div>
+                <ListBids inscriptionId={inscriptionId} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
